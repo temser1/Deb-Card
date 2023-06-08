fbff8554ab9f5e5613d37b2c0aacddc283bed57f
Serovtsev Artem <1doncarleon@gmail.com>
2023-06-08 21:49:44 +0300

    add

--- src/test/java/ru/netology/DebCardTest.java
+++ src/test/java/ru/netology/DebCardTest.java
@@ -1,6 +1,6 @@
 package ru.netology;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
